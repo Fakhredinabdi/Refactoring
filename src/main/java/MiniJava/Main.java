de8aec44e8d3c3f7f2d8720d8b122ba967031b5e
Hossein-AliHosseini <themmdhossein@gmail.com>
2025-05-26 16:59:22 +0330

    Apply Extract method

--- src/main/java/MiniJava/Main.java
+++ src/main/java/MiniJava/Main.java
@@ -4,6 +4,10 @@ import MiniJava.compiler.CompilerFacade;
 
 public class Main {
     public static void main(String[] args) {
+        compileSourceCode();
+    }
+
+    private static void compileSourceCode() {
         CompilerFacade compiler = new CompilerFacade();
         compiler.compile("src/main/resources/code");
     }
